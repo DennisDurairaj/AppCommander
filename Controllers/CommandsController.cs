25200e8e6b3b3cc55721f66f3923f15e2e182164
Dennis Durairaj <dennis.durairaj@dnvgl.com>
2020-06-30 14:52:23 +0200

    Create commands api controller

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using AppCommander.Data;
+using AppCommander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCommander.Controllers
+{
+  [Route("api/commands")]
+  [ApiController]
+  public class CommandsController : ControllerBase
+  {
+    private readonly IAppCommanderRepo _repository;
+
+    public CommandsController(IAppCommanderRepo repository)
+    {
+      _repository = repository;
+    }
+    //private readonly MockAppCommanderRepo _repository = new MockAppCommanderRepo();
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+      var commandItems = _repository.GetAppCommands();
+      return Ok(commandItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+      var commandItem = _repository.GetCommandById(id);
+      return Ok(commandItem);
+    }
+  }
+}
